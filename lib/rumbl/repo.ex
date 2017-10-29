c016853e47bdbdec4b2e21dfb2e9ce1e1f3cd55e
Lee Sigauke <leesigs@gmail.com>
2017-10-29 20:51:39 +0000

    Manage users and migrate database - chapter 4

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,33 +1,3 @@
 defmodule Rumbl.Repo do
-  # use Ecto.Repo, otp_app: :rumbl
-  @moduledoc """
-  In memory repository
-  """
-
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: "1", name: "Jose", password: "elixir"},
-      %Rumbl.User{id: "2", name: "Bruce", password: "7langs"},
-      %Rumbl.User{id: "3", name: "Chris", password: "phx"},
-    ]
-  end
-  def all(_module), do: []
-
-  def get(module, id) do
-    module
-    |> all()
-    |> Enum.find(fn map -> 
-        map.id == id
-    end)
-  end 
-
-  def get_by(module, params) do
-    module
-    |> all()
-    |> Enum.find(fn map ->
-        Enum.all?(params, fn {key, val} -> 
-          Map.get(map, key) == val 
-        end)
-    end)
-  end 
+  use Ecto.Repo, otp_app: :rumbl
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,3 +1,20 @@
 defmodule Rumbl.User do
-  defstruct [:id, :name, :password]
+  use Rumbl.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end 
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+  end
+
 end
